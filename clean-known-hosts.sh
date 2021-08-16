abb58c6b12d0d7562d4441bbafeb537ff8d0df05
Fabian Lee <flee999@hotmail.com>
2021-08-16 18:37:43 -0400

    dynamically lookup ip address from terraform variables file

--- clean-known-hosts.sh
+++ clean-known-hosts.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+#
+# removes key from ssh known_hosts in case of host recreation
 
-ssh-keygen -f ~/.ssh/known_hosts -R 192.168.140.7
+# terraform variables files
+tfvars=terraform.tfvars
+
+# pulls terraform variable values out of file
+function getTFVal() {
+  grep $1 $tfvars | awk '{print $3}' | sed 's/"//g'
+}
+
+############## MAIN ##################
+
+# vCenter host
+jumphost_ip=$(getTFVal jumphost_ip)
+
+ssh-keygen -f ~/.ssh/known_hosts -R $jumphost_ip

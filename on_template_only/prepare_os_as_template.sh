a45aa1f4be28be33f1c0811d005714b911f163cf
Fabian Lee <flee999@hotmail.com>
2021-11-18 13:52:02 -0500

    fixed issue if file does not exist

--- on_template_only/prepare_os_as_template.sh
+++ on_template_only/prepare_os_as_template.sh
@@ -15,10 +15,12 @@ echo net.ipv6.conf.default.disable_ipv6=1 | sudo tee -a /etc/sysctl.conf
 echo net.ipv6.conf.lo.disable_ipv6=1 | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
-sudo sed -i 's/preserve_hostname: false/preserve_hostname: true/g' /etc/cloud/cloud.cfg
+if [ -f /etc/cloud/cloud.cfg ]; then
+  sudo sed -i 's/preserve_hostname: false/preserve_hostname: true/g' /etc/cloud/cloud.cfg
+fi
 
 ls -l /etc/netplan
-sudo rm /etc/netplan/*.yaml
+sudo rm -f /etc/netplan/*.yaml
 
 rm prepare_template.sh
 
